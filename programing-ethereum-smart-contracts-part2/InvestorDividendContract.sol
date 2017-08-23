e144e1fec8cac66ed899a871d95813c063abbbc5
garfieldsg <szymon@gaertig.com.pl>
2017-08-23 19:31:40 +0200

    fixes

--- programing-ethereum-smart-contracts-part2/InvestorDividendContract.sol
+++ programing-ethereum-smart-contracts-part2/InvestorDividendContract.sol
@@ -1,10 +1,10 @@
 pragma solidity^0.4.0;
 
 contract InvestorDividendContract {
-    
     mapping(address => uint) _shares;
-
-    address _simonAddress = 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db;
+    mapping(address => bool) _permissions;
+    
+    address _simonAddress = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
     address _pawelAddress = 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c;
     address _mikolajAddress = 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db;
     
@@ -14,15 +14,43 @@ contract InvestorDividendContract {
         _shares[_mikolajAddress] = 33;
     }
     
+    modifier OnlyForPartner() {
+        if(_shares[msg.sender] == 0){
+            throw;
+        }
+        _;
+    }
+    
     function Donate() payable{
         // this function doesnt need an implementation
         // payable keyword guarantee us, that this function will accept payment
     }
 
+    function IsWithdrawPermitted() private returns (bool){
+        return _permissions[_simonAddress] 
+                && _permissions[_pawelAddress] 
+                && _permissions[_mikolajAddress];
+    }
+    
+    function ResetPermissions() private {
+        _permissions[_simonAddress] = false;
+        _permissions[_pawelAddress] = false;
+        _permissions[_mikolajAddress] = false;
+    }
+    
     function Withdraw() returns (string){
+        
+        _permissions[msg.sender] = true;
+        
+        if(!IsWithdrawPermitted()){
+            return("All partners should ask for withdraw");
+        }
+        
         _simonAddress.transfer(CalculateDividend(_simonAddress));
         _pawelAddress.transfer(CalculateDividend(_pawelAddress));
         _mikolajAddress.transfer(CalculateDividend(_mikolajAddress));
+        
+        ResetPermissions();
         
         return "OK";
     }
@@ -31,7 +59,7 @@ contract InvestorDividendContract {
         return  this.balance *  (_shares[beneficientAddress] / 100);
     }
     
-    function GetBalance() constant returns (uint){
+    function GetBalance() OnlyForPartner constant returns (uint){
         return this.balance;
     }
 }
---
+++ programing-ethereum-smart-contracts-part2/README.md
@@ -0,0 +1,2 @@
+# GarfsgAboutCodeBlog
+Code sample for article: http://garfsgaboutcode.com/2017/08/23/programing-ethereum-smart-contracts-part-2/
